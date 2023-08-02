7446fd1d0821a311779ad1f632ccb9fe892e5eed
pk46 <kupcik46@gmail.com>
2023-08-02 10:07:16 +0200

    Generovani html pro vsechny restaurace - refactoring main - uprava sablony - uprava html_generator

--- src/utilities/html_generator.py
+++ src/utilities/html_generator.py
@@ -1,15 +1,12 @@
 from jinja2 import Environment, FileSystemLoader
+import os
 
 
 class HTMLGenerator:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
         self.env = Environment(loader=FileSystemLoader("C:/Users/motor/PycharmProjects/Obedy/src/template", "utf-8"))
         self.template = self.env.get_template("template.html")
     
     def generate_html(self, data):
-        rendererd_html = self.template.render(
-            title=self.name,
-            data=data[0]
-        )
-        return rendererd_html
+        rendered_html = self.template.render(restaurants=data)
+        return rendered_html

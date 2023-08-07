da44cea4b03f0b2bb6ab407af159cc19d06c250f
pk46 <kupcik46@gmail.com>
2023-08-07 16:55:36 +0200

    Logging + oprava nevyvovalavani vyjimky RetryableHttpError - funkcni logging do souboru i do konzole - refactoring request_helper.py

--- src/utilities/request_helper.py
+++ src/utilities/request_helper.py
@@ -1,13 +1,19 @@
 import httpx
+import logging
 
 
 class RetryableHttpError(Exception):
+    
     def __init__(self, message, status_code):
         super().__init__(message)
         self.status_code = status_code
 
 
-async def get_url(url, max_retries=5):
+logging.basicConfig(level=logging.INFO)
+
+
+async def get_url(url, class_name=None):
+    max_retries = 5
     for retry in range(1, max_retries + 1):
         try:
             async with httpx.AsyncClient() as client:
@@ -15,18 +21,21 @@ async def get_url(url, max_retries=5):
                 response.raise_for_status()
                 return response.text, response.status_code
         except httpx.RequestError:
-            print(f"Pokus č. {retry} selhal chybou RequestError")
-        except httpx.HTTPStatusError as http_error:
-            if 500 <= http_error.response.status_code < 600:
-                print(f"Pokus č. {retry} selhal kvůli chybě {http_error.response.status_code}")
-            elif 400 <= http_error.response.status_code < 500:
-                print(f"Pokus č. {retry} kvůli chybě {http_error.response.status_code}")
+            logging.warning(f"{class_name}: pokus č. {retry} selhal chybou RequestError")
             if retry == max_retries:
-                raise RetryableHttpError(f"Všechny pokusy selhaly chybou. {http_error}",
-                                         http_error.response.status_code)
-        except Exception as error:
-            print(f"Pokus č. {retry} selhal chybou: {error}")
-            if retry == max_retries:
-                raise
+                raise RetryableHttpError("", "RequestError")
             else:
                 continue
+        except httpx.HTTPStatusError as http_error:
+            if 500 <= http_error.response.status_code < 600:
+                logging.warning(f"{class_name}: pokus č. {retry} selhal kvůli chybě {http_error.response.status_code}")
+                if retry == max_retries:
+                    raise RetryableHttpError("", http_error)
+            elif 400 <= http_error.response.status_code < 500:
+                logging.warning(f"{class_name} pokus č. {retry} kvůli chybě {http_error.response.status_code}")
+                if retry == max_retries:
+                    raise RetryableHttpError("", http_error.response.status_code)
+        except Exception as error:
+            logging.warning(f"{class_name} pokus č. {retry} selhal chybou: {error}")
+            if retry == max_retries:
+                raise RetryableHttpError("", error)

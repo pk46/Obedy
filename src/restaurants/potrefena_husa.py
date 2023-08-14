2a02ce9b397ed7dff91a8fdbe907a73b53ddd3c3
pk46 <kupcik46@gmail.com>
2023-08-14 18:14:55 +0200

    Potrefena Husa funkcni scrape - optimalizace

--- src/restaurants/potrefena_husa.py
+++ src/restaurants/potrefena_husa.py
@@ -1,7 +1,7 @@
-import time
+import logging
 
 from bs4 import BeautifulSoup
-from selenium.common import TimeoutException
+from selenium.common import TimeoutException, WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -9,36 +9,67 @@ from selenium.webdriver.support import expected_conditions as ec
 from restaurants.restaurant import Restaurant
 from selenium import webdriver
 
+from utilities.request_helper import RetryableHttpError
+
 
 class PotrefenaHusa(Restaurant):
+    __MAX_RETRIES = 5
+    
     def __init__(self, url, name):
         super().__init__(url, name)
+        logging.basicConfig(filename="../scraper.log",
+                            format=self.FORMAT,
+                            level=logging.INFO,
+                            encoding="utf-8",
+                            filemode="a")
+        self.logger = logging.getLogger()
+        self.__options = webdriver.EdgeOptions()
+        self.__options.add_argument('--headless')
+        self.__browser = None
+    
+    async def __get_url(self, url):
+        for retry in range(1, self.__MAX_RETRIES + 1):
+            try:
+                self.__browser = webdriver.Edge(options=self.__options)
+                self.logger.info(f"{self._name}: Selenium headless browser started")
+                self.__browser.get(url)
+                self.__browser.switch_to.frame(0)  # iFrame with food is on index 0
+                
+                elements = WebDriverWait(self.__browser, 10).until(
+                    ec.presence_of_all_elements_located((By.CLASS_NAME, "men-integ-web__day"))
+                )
+                return self._process_data(self.__browser.page_source)
+            except TimeoutException:
+                logging.error(f"{self._name}: požadovaný element nebyl na stránce nalezen ")
+                if retry == self.__MAX_RETRIES:
+                    raise RetryableHttpError("Všechny pokusy selhaly vyjímkou. Poslední: ", "WebDriverException")
+            except WebDriverException as e:
+                logging.error(f"{self._name}: pokus č. {retry} selhal chybou WebDriverException {e.msg}")
+                if retry == self.__MAX_RETRIES:
+                    raise RetryableHttpError(f"Všechny pokusy selhaly vyjímkou. Poslední:  {e.msg}", self.__class__)
+            except Exception as error:
+                logging.error(f"{self._name} pokus č. {retry} selhal chybou: {error}")
+                return None
+            finally:
+                self.__browser.quit()
+                self.logger.info(f"{self.name}: Selenium headless browser exited.")
     
     def _process_data(self, data):
-        date = data[0].find("div", {"class": "row justify-content-center flex-wrap"}).find("p")
-        week_food = data[0].find("div", {"class": "men-integ-web__foods-wrapper"})
-        # print(data)
-        ...
-    
+        menu = BeautifulSoup(data, "lxml")
+        days = menu.findAll("div", {"class": "men-integ-web__day"})[:-2]  # -2 = except Weekend
+        for i in range(len(days)):
+            daily_menu = days[i]
+            # print(week_menu)
+            day_value = daily_menu.find("h3", {"class": "men-integ-web__subtitle"}).text
+            food_elements = daily_menu.findAll("span", {"class": "men-integ-web__food__name"})
+            food_value = [food.text for food in food_elements]
+            
     async def main(self):
-        options = webdriver.EdgeOptions()
-        options.add_argument('--headless')
-        browser = webdriver.Edge(options=options)
-        
         try:
-            browser.get(self._url)
-            browser.switch_to.frame(0)
-            elements = WebDriverWait(browser, 10).until(
-                ec.presence_of_all_elements_located((By.CLASS_NAME, "men-integ-web__day"))
-            )
-            for element in elements[:-2]:
-                print(element.text)
-        except TimeoutException:
-            print("I give up...")  # nahradit loggerem
-        finally:
-            browser.quit()
-        scraped_data = await self._scrape_data()
-        # if scraped_data:
-        #     self._process_data(scraped_data)
-        # else:
-        #     self._menu["Chyba"] = ["Chyba při načítání dat"]
+            scraped_data = await self.__get_url(self._url)
+            if scraped_data:
+                self._process_data(scraped_data)
+        except Exception as e:
+            logging.error(f"Chyba při načítání dat: {e}")
+            self._menu["Chyba"] = ["Chyba při načítání dat"]
+
